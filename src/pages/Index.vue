0e7787e27df17f0b7059b12ee2f68ae62a1d30bd
Pieter van Halem <pietervanhalem@hotmail.com>
2021-10-21 13:56:28 +0200

    init repo

---
+++ src/pages/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <q-btn label='notify as in documentation' @click='showNotif'/>
+    <q-btn label='notify from Alias' @click='showNotify2'/>
+  </q-page>
+</template>
+
+<script>
+import { useQuasar } from 'quasar';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  setup() {
+    // from https://quasar.dev/quasar-plugins/notify
+    const $q = useQuasar();
+
+    return {
+      showNotif() {
+        $q.notify({
+          message: 'Jim pinged you.',
+          icon: 'announcement',
+        });
+      },
+    };
+  },
+
+  methods: {
+    showNotify2() {
+      this.$q.notify({
+        message: 'Jim pinged you.',
+        icon: 'announcement',
+      });
+    },
+  },
+});
+</script>
